889a7d69e83de4b008079de96e1ad5f03283c8dc
Sander van Kasteel <sander@hotelchamp.com>
2016-12-15 22:32:08 +0100

    Kinda managed to finish path detection for Spotify running on OS X / macOS

--- patcher/js/spotify.js
+++ patcher/js/spotify.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 var Spotify = function () {
     this.platform = process.platform;
+    this.version = null;
+    this._installDir = null;
 
     this.getVersion = function() {
+        if(this.platform == 'darwin') {
+            // system
+            return "1.4.1234";
+        }
+
         return "1.0.43.125.g376063c5";
     }
 
@@ -23,13 +31,31 @@ var Spotify = function () {
     }
 
     this._installationDirectory = function() {
+        if(this._installDir != null) {
+            return this._installDir;
+        }
+
         switch (this.platform) {
             case 'linux':
-                return '/usr/share/spotify';
+                this._installDir = '/usr/share/spotify';
             case 'darwin':
-                return '/Applications/Spotify.app/Contents/Resources';
+                try {
+                    var that = this; // Because dirty fixes :(
+                    exec('osascript -e \'tell application "System Events" to POSIX path of (file of process "Spotify")\'', function(err, stdout, stderr) {
+                        if(err) {
+                            throw Error;
+                        }
+                        that._installDir = stdout;       
+                    });
+                } catch(err) {
+                    this._installDir = null;;
+                }
+            case 'win32':
+                this._installDir = 'C:\\';
             default:
-                return 'C:\\';
+                this._installDir = null;
         }
+
+        return this._installDir;
     }
 }
