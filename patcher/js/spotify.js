bab54c559da94ad8d1de20b6414121fab4d95fd7
Sander van Kasteel <info@sandervankasteel.nl>
2016-12-15 02:09:43 +0100

    Baby steps for detecting the current Spotify installation

---
+++ patcher/js/spotify.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+var Spotify = function () {
+    this.platform = process.platform;
+
+    this.getVersion = function() {
+        return "1.0.43.125.g376063c5";
+    }
+
+    // Returns if the current version of Spotify is already patched with Spotio ^^
+    this.isPatched = function() {
+        try {
+            fs.accessSync(this._installationDirectory() + '/Apps/spotio');
+            return true;    
+        } catch (err) {
+            return false;
+        }
+    }
+
+
+    this.markAsPatched = function() {
+        fs.writeFile(this._installationDirectory() + '/Apps/spotio');
+    }
+
+    this._installationDirectory = function() {
+        switch (this.platform) {
+            case 'linux':
+                return '/usr/share/spotify';
+            case 'darwin':
+                return '~/Spotify.app';
+            default:
+                return 'C:\\';
+        }
+    }
+}
